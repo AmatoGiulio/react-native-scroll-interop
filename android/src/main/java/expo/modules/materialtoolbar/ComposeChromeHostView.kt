eaf63cc7ebf3178baacf6ae073606008b313723a
Giulio Amato <giulioamato19.92@gmail.com>
2026-08-13 21:59:34 +0200

    Bridge native floating placement from chrome host

--- android/src/main/java/expo/modules/materialtoolbar/ComposeChromeHostView.kt
+++ android/src/main/java/expo/modules/materialtoolbar/ComposeChromeHostView.kt
@@ -16,54 +16,48 @@ abstract class ComposeChromeHostView(
   context: Context,
   appContext: AppContext,
 ) : ExpoView(context, appContext), ReactPointerEventsView {
+  final override val pointerEvents get() = PointerEvents.BOX_NONE
 
-  final override val pointerEvents: PointerEvents
-    get() = PointerEvents.BOX_NONE
-
-  protected val composeView: ComposeView = ComposeView(context).apply {
+  protected val composeView = ComposeView(context).apply {
     setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
   }
 
-  private var hostMeasurePending = false
-  private var lastNativeImeVisible: Boolean? = null
-  private var lastTraceIncomingImeVisible: Boolean? = null
-  private var lastRootImeVisible: Boolean? = null
+  private var measurePending = false
+  private var lastNativeIme: Boolean? = null
+  private var lastTraceIme: Boolean? = null
+  private var lastRootIme: Boolean? = null
 
   init {
     isClickable = false
     isFocusable = false
     addView(composeView)
+    addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+      NativeFloatingToolbarPlacement.afterLayout(this, composeView)
+    }
   }
 
   protected abstract fun onMeasureComposeChild(hostWidthPx: Int, hostHeightPx: Int)
-
   protected open fun onNativeImeVisibilityChanged(visible: Boolean) = Unit
 
   protected fun scheduleHostMeasureAndLayout() {
-    if (hostMeasurePending) return
-    hostMeasurePending = true
+    if (measurePending) return
+    measurePending = true
     post {
       if (!isAttachedToWindow || width <= 0 || height <= 0) {
-        hostMeasurePending = false
+        measurePending = false
         return@post
       }
-
-      val hostWidth = width
-      val hostHeight = height
-      onMeasureComposeChild(hostWidth, hostHeight)
+      onMeasureComposeChild(width, height)
       onLayout(false, left, top, right, bottom)
-      hostMeasurePending = false
-
-      if (composeView.isLayoutRequested) {
-        scheduleHostMeasureAndLayout()
-      }
+      NativeFloatingToolbarPlacement.afterLayout(this, composeView)
+      measurePending = false
+      if (composeView.isLayoutRequested) scheduleHostMeasureAndLayout()
     }
   }
 
   override fun requestLayout() {
     super.requestLayout()
-    if (!isAttachedToWindow) return
-    scheduleHostMeasureAndLayout()
+    if (isAttachedToWindow) scheduleHostMeasureAndLayout()
   }
 
   override fun onAttachedToWindow() {
@@ -73,78 +67,55 @@ abstract class ComposeChromeHostView(
 
   override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
     super.onSizeChanged(w, h, oldw, oldh)
-    if (!isAttachedToWindow || w <= 0 || h <= 0) return
-    scheduleHostMeasureAndLayout()
+    if (isAttachedToWindow && w > 0 && h > 0) scheduleHostMeasureAndLayout()
   }
 
   override fun onApplyWindowInsets(insets: android.view.WindowInsets): android.view.WindowInsets {
     val applied = super.onApplyWindowInsets(insets)
-    val incomingImeVisible = WindowInsetsCompat
-      .toWindowInsetsCompat(applied, this)
-      .isVisible(WindowInsetsCompat.Type.ime())
-
-    // Let Compose consume the new inset snapshot first. On IME close this means the toolbar can be
-    // made visible only after the child has already received the non-IME geometry.
+    val compat = WindowInsetsCompat.toWindowInsetsCompat(applied, this)
+    val ime = compat.isVisible(WindowInsetsCompat.Type.ime())
     composeView.dispatchApplyWindowInsets(applied)
-
-    if (incomingImeVisible != lastNativeImeVisible) {
-      lastNativeImeVisible = incomingImeVisible
-      onNativeImeVisibilityChanged(incomingImeVisible)
+    NativeFloatingToolbarPlacement.windowInsets(this, compat)
+    if (ime != lastNativeIme) {
+      lastNativeIme = ime
+      onNativeImeVisibilityChanged(ime)
     }
-
-    traceImeInsets(applied)
+    traceInsets(applied)
     scheduleHostMeasureAndLayout()
     return applied
   }
 
-  private fun traceImeInsets(insets: android.view.WindowInsets) {
+  private fun traceInsets(insets: android.view.WindowInsets) {
     if (!NativeScrollTracing.enabled) return
-
-    val incomingImeVisible = WindowInsetsCompat
-      .toWindowInsetsCompat(insets, this)
+    val ime = WindowInsetsCompat.toWindowInsetsCompat(insets, this)
       .isVisible(WindowInsetsCompat.Type.ime())
-    val rootImeVisible = ViewCompat
-      .getRootWindowInsets(this)
+    val rootIme = ViewCompat.getRootWindowInsets(this)
       ?.isVisible(WindowInsetsCompat.Type.ime())
-
-    if (
-      incomingImeVisible == lastTraceIncomingImeVisible &&
-      rootImeVisible == lastRootImeVisible
-    ) {
-      return
-    }
-
-    lastTraceIncomingImeVisible = incomingImeVisible
-    lastRootImeVisible = rootImeVisible
-    logImeSnapshot("apply", incomingImeVisible, rootImeVisible)
-
+    if (ime == lastTraceIme && rootIme == lastRootIme) return
+    lastTraceIme = ime
+    lastRootIme = rootIme
+    logInsets("apply", ime, rootIme)
     post {
       if (!isAttachedToWindow || !NativeScrollTracing.enabled) return@post
-      val postedRootImeVisible = ViewCompat
-        .getRootWindowInsets(this)
-        ?.isVisible(WindowInsetsCompat.Type.ime())
-      logImeSnapshot("post", incomingImeVisible, postedRootImeVisible)
+      logInsets(
+        "post",
+        ime,
+        ViewCompat.getRootWindowInsets(this)?.isVisible(WindowInsetsCompat.Type.ime()),
+      )
     }
   }
 
-  private fun logImeSnapshot(
-    phase: String,
-    incomingImeVisible: Boolean,
-    rootImeVisible: Boolean?,
-  ) {
+  private fun logInsets(phase: String, ime: Boolean, rootIme: Boolean?) {
     val visibility = when (composeView.visibility) {
       View.VISIBLE -> "VISIBLE"
       View.INVISIBLE -> "INVISIBLE"
       View.GONE -> "GONE"
       else -> composeView.visibility.toString()
     }
-
     Log.d(
       NATIVE_SCROLL_LOG_TAG,
-      "CHROME_IME phase=$phase hostClass=${javaClass.simpleName} " +
-        "incoming=$incomingImeVisible root=$rootImeVisible " +
-        "host=${width}x${height} " +
-        "composeMeasured=${composeView.measuredWidth}x${composeView.measuredHeight} " +
+      "CHROME_IME phase=$phase hostClass=${javaClass.simpleName} incoming=$ime root=$rootIme " +
+        "host=${width}x${height} composeMeasured=${composeView.measuredWidth}x${composeView.measuredHeight} " +
         "composeBounds=${composeView.left},${composeView.top},${composeView.right},${composeView.bottom} " +
         "composeVisibility=$visibility layoutRequested=${composeView.isLayoutRequested}",
     )
@@ -154,8 +125,6 @@ abstract class ComposeChromeHostView(
     val width = View.MeasureSpec.getSize(widthMeasureSpec)
     val height = View.MeasureSpec.getSize(heightMeasureSpec)
     setMeasuredDimension(width, height)
-
-    if (!isAttachedToWindow) return
-    onMeasureComposeChild(width, height)
+    if (isAttachedToWindow) onMeasureComposeChild(width, height)
   }
 }
