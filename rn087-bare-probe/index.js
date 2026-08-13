1f77ba3b616cd51c36525a87d887979a31afa9af
Giulio Amato <giulioamato19.92@gmail.com>
2026-08-13 12:17:43 +0200

    Register isolated lifecycle probe entry

--- rn087-bare-probe/index.js
+++ rn087-bare-probe/index.js
@@ -1,5 +1,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
+import AppLifecycle from './AppLifecycle';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('RN087NestedScrollLifecycleProbe', () => AppLifecycle);
