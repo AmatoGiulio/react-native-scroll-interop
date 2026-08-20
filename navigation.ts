282a1a4fee36ac851d2cb7a81adb38a0810a0754
Giulio Amato <giulioamato19.92@gmail.com>
2026-08-20 17:11:55 +0200

    feat: expose navigator-neutral Material3 mapper

---
+++ navigation.ts
@@ -0,0 +1,14 @@
+export {
+  hasUnsupportedMaterial3HeaderOptions,
+  resolveMaterial3Navigation,
+  resolveMaterial3TopAppBarDescriptor,
+} from './src/navigation/material3NavigationMapper';
+
+export type {
+  Material3NavigationDecision,
+  Material3NavigationOptionBag,
+  Material3NavigationScope,
+  Material3StackNavigationOptions,
+  Material3TopAppBarDescriptor,
+  Material3TopAppBarNavigationOptions,
+} from './src/navigation/material3NavigationMapper';
