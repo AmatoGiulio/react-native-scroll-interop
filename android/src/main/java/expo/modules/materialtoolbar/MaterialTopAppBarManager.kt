0607bb42e8f467980facb97bae888af115e0c258
Giulio Amato <giulioamato19.92@gmail.com>
2026-08-20 11:15:31 +0200

    refactor(android): register Material TopAppBar with React Native

---
+++ android/src/main/java/expo/modules/materialtoolbar/MaterialTopAppBarManager.kt
@@ -0,0 +1,48 @@
+package expo.modules.materialtoolbar
+
+import com.facebook.react.uimanager.SimpleViewManager
+import com.facebook.react.uimanager.ThemedReactContext
+import com.facebook.react.uimanager.annotations.ReactProp
+
+internal class MaterialTopAppBarManager : SimpleViewManager<ExpoMaterialTopAppBarView>() {
+  override fun getName(): String = "RNSIMaterialTopAppBar"
+
+  override fun createViewInstance(
+    reactContext: ThemedReactContext,
+  ): ExpoMaterialTopAppBarView = ExpoMaterialTopAppBarView(reactContext)
+
+  @ReactProp(name = "title")
+  fun setTitle(view: ExpoMaterialTopAppBarView, value: String?) = view.setTitle(value.orEmpty())
+
+  @ReactProp(name = "visible", defaultBoolean = true)
+  fun setVisible(view: ExpoMaterialTopAppBarView, value: Boolean) = view.setVisibleState(value)
+
+  @ReactProp(name = "variant")
+  fun setVariant(view: ExpoMaterialTopAppBarView, value: String?) = view.setVariant(value ?: "medium")
+
+  @ReactProp(name = "scrollBehavior")
+  fun setScrollBehavior(view: ExpoMaterialTopAppBarView, value: String?) =
+    view.setScrollBehavior(value ?: "none")
+
+  @ReactProp(name = "navigationIcon")
+  fun setNavigationIcon(view: ExpoMaterialTopAppBarView, value: String?) =
+    view.setNavigationIcon(value ?: "none")
+
+  @ReactProp(name = "navigationAccessibilityLabel")
+  fun setNavigationAccessibilityLabel(view: ExpoMaterialTopAppBarView, value: String?) =
+    view.setNavigationAccessibilityLabel(value ?: "Back")
+
+  @ReactProp(name = "themeMode")
+  fun setThemeMode(view: ExpoMaterialTopAppBarView, value: String?) =
+    view.setThemeMode(value ?: "system")
+
+  @ReactProp(name = "dynamicColor", defaultBoolean = false)
+  fun setDynamicColor(view: ExpoMaterialTopAppBarView, value: Boolean) = view.setDynamicColor(value)
+
+  override fun getExportedCustomDirectEventTypeConstants(): MutableMap<String, Any> =
+    mutableMapOf(
+      "topNavigationPress" to mutableMapOf(
+        "registrationName" to "onNavigationPress",
+      ),
+    )
+}
