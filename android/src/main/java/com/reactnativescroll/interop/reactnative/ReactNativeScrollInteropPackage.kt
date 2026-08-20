0bf8f9f6d60dc660e99bbd95abcd66e7ec1b63eb
Giulio Amato <giulioamato19.92@gmail.com>
2026-08-20 17:27:15 +0200

    refactor: wire neutral RN and Material3 managers

--- android/src/main/java/com/reactnativescroll/interop/reactnative/ReactNativeScrollInteropPackage.kt
+++ android/src/main/java/com/reactnativescroll/interop/reactnative/ReactNativeScrollInteropPackage.kt
@@ -4,9 +4,8 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
-import expo.modules.materialtoolbar.MaterialToolbarManager
-import expo.modules.materialtoolbar.MaterialTopAppBarManager
-import expo.modules.materialtoolbar.ReactNativeNestedScrollHostManager
+import com.reactnativescroll.interop.material3.ui.MaterialToolbarManager
+import com.reactnativescroll.interop.material3.ui.MaterialTopAppBarManager
 
 /** Standard React Native package surface. No Expo Modules runtime is required. */
 class ReactNativeScrollInteropPackage : ReactPackage {
