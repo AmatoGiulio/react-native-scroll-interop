493009c5303006b93ef16c28b033989f6242ab71
Giulio Amato <giulioamato19.92@gmail.com>
2026-08-23 20:41:45 +0200

    style(expo): enable dynamic color on Home app bar

--- examples/expo/app/navigation-first/(tabs)/home/_layout.tsx
+++ examples/expo/app/navigation-first/(tabs)/home/_layout.tsx
@@ -12,6 +12,7 @@ export default function HomeStackLayout() {
             topAppBar: {
               variant: 'large',
               scrollBehavior: 'exitUntilCollapsed',
+              dynamicColor: true,
             },
           },
         }}
