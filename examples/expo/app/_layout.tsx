6fbd9f6aaa8f2babb77bf49f2e0b142710773169
Giulio Amato <giulioamato19.92@gmail.com>
2026-08-23 20:40:31 +0200

    style(expo): let status bar follow system theme

--- examples/expo/app/_layout.tsx
+++ examples/expo/app/_layout.tsx
@@ -4,7 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 export default function RootLayout() {
   return (
     <>
-      <StatusBar style="light" hidden={false} />
+      <StatusBar style="auto" hidden={false} />
       <Stack screenOptions={{ headerShown: false }} />
     </>
   );
