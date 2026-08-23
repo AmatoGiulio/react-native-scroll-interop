13bd34a14f68a660f9aa7d1ba9cb33a1db333771
Giulio Amato <giulioamato19.92@gmail.com>
2026-08-23 20:40:48 +0200

    style(expo): use dynamic Material roles on Home

--- examples/expo/app/navigation-first/(tabs)/home/index.tsx
+++ examples/expo/app/navigation-first/(tabs)/home/index.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'expo-router';
 import { Pressable, ScrollView, StyleSheet, Text } from 'react-native';
 
-import { material3Dark as colors } from '../../../../theme';
+import { material3Dynamic as colors } from '../../../../theme';
 
 const ROWS = Array.from({ length: 80 }, (_, index) => `Home row ${index + 1}`);
 
